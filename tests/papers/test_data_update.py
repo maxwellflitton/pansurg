a1f9d7153815b00929cb4419382f90db72158a94
Archives <archives@Archivess-MacBook-Pro.local>
2020-05-04 16:46:03 +0100

    should fail tests

--- tests/papers/test_data_update.py
+++ tests/papers/test_data_update.py
@@ -10,7 +10,7 @@ class TestDataUpdate(TestCase):
         test = DataUpdate()
 
         self.assertEqual(str, type(test._url))
-        self.assertEqual([], test.data)
+        self.assertEqual(["fail"], test.data)
 
     @patch("pansurg.papers.data_update.tarfile")
     @patch("pansurg.papers.data_update.urllib")
